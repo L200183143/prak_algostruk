087d7725906e17b4644a2f4e59588b3f08aeaf99
L200183143 <45041287+L200183143@users.noreply.github.com>
2020-04-25 13:50:02 +0700

    Add files via upload

---
+++ L200183143_Modul7_H/no2.py
@@ -0,0 +1,7 @@
+import re
+
+f = open('Indonesia.txt', 'r', encoding='latin1')
+p = r'di[\w\.-]+'
+string = re.findall(p, f.read())
+print(string)
+print ("----------------------------------------------------------------------------")
---
+++ L200183143_Modul7_H/no3.py
@@ -0,0 +1,7 @@
+import re
+
+f = open('Indonesia.txt', 'r', encoding='latin1')
+r = r'di\s[\w\.-]+'
+string3 = re.findall(r, f.read())
+print(string3)
+print ("-------------------------------------------------------------------------")
